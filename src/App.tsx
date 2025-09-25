1bd0a40a3f453606c1045d82504dd235e772b472
JirathWanna <jarijirath@gmail.com>
2025-09-25 22:02:02 +0700

    Navbar and main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import Jirath from './assets/Gemini_Generated_Image_v9znvvv9znvvv9zn.png'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div className='text-center font-bold text-9xl'>
-      Jirath Wanna
+    <div>
+      <Navbar></Navbar>
+      <div className='flex justify-between mt-20 px-[25%]'>
+        <div>
+          <div className='text-[5vw] font-bold'>Hi There!</div>
+          <div className='text-gray-600 text-2xl mb-6'>I'm a student from Joseph Upatham School looking forward to learn Computer Science!</div>
+          <div className='text-gray-500 mb-10'>Since 2005, I've enjoyed turning complex problems into simple, beautiful and intuitive designs. When I'm not pushing pixels, you'll find me cooking, gardening or working out in the park.</div>
+          <button className='bg-blue-500 text-white font-medium p-3 rounded-2xl cursor-pointer hover:bg-blue-400 duration-100'>See My Work!</button>
+        </div>
+        <img className='w-48 md:w-96' src={Jirath} alt="" />
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+
+export default function() {
+    return (
+        <div className="p-6 bg-black text-white flex justify-between">
+            <div className="text-2xl font-bold">Jirath Wanna</div>
+            <div className="flex space-x-5">
+                <div>Facebook</div>
+                <div>IG</div>
+                <div>Github</div>
+            </div>
+        </div>
+    )
+}
